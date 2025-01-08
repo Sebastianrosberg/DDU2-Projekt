006da4b5cccde9ef94e07ca47a69c04f08820377
Sebastianrosberg <sebbe.rosberg@gmail.com>
2025-01-08 11:07:25 +0100

    AddUpTo
    Jag gör en click funktion på find knappen där jag loopar igenom två loops av mina divar för att sedan kunna jämföra om i och j blir de inskrivna value. Jag breakar sedan inre loopen och sedan yttre näär jag hittar vardera.

---
+++ AddUpTo/index.js
@@ -0,0 +1,24 @@
+const findButton = document.getElementById("findButton");
+const valueOfTwoCells = document.getElementById("sumOfTwoCells")
+
+
+findButton.addEventListener("click", function () {
+    let foundMatch = false
+    for (let i = 0; i < divsInMain.length; i++) {
+        for (let j = i + 1; j < divsInMain.length; j++) {
+
+            let firstTerm = Number(divsInMain[i].textContent)
+            let secondTerm = Number(divsInMain[j].textContent)
+
+            if (firstTerm + secondTerm == Number(valueOfTwoCells.value)) {
+                divsInMain[i].style.backgroundColor = "lightgreen"
+                divsInMain[j].style.backgroundColor = "lightgreen"
+                foundMatch = true;
+                break;
+            }
+        }
+        if (foundMatch) {
+            break
+        }
+    }
+})
